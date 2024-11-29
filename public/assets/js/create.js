9de8d0fdf46e13ee79557ebf57d4f8c16685e8fb
Athen Lee <cupid.jack505@gmail.com>
2024-11-29 11:57:08 -0500

    fix: modify editor with resizable

--- public/assets/js/create.js
+++ public/assets/js/create.js
@@ -55,7 +55,6 @@ jsEditor.on("change", function (delta) {
     inputHandler();
 });
 
-
 $("#save-btn").click(function () {
     let html = htmlEditor.getValue();
     let css = cssEditor.getValue();
@@ -89,4 +88,82 @@ $("#save-btn").click(function () {
             console.log('Request failed:', error);
         }
     });
-})
+});
+
+
+let editorHeight = (window.innerHeight - 98 - 96) / 3;
+$(".editor").css("height", editorHeight);
+// Custom resize handle
+// For resizing rows within the left container
+var isResizingRows = false;
+var lastY = 0;
+var prevHeight = 0;
+var nextHeight = 0;
+var prevRow = null;
+var nextRow = null;
+
+$(".handler").on("mousedown", function (e) {
+    isResizingRows = true;
+    lastY = e.pageY;
+
+    prevRow = $(this).prev();
+    nextRow = $(this).next();
+
+    prevHeight = prevRow.height();
+    nextHeight = nextRow.height();
+
+    e.preventDefault();
+});
+
+$(document).on("mousemove", function (e) {
+    if (isResizingRows) {
+        var diff = e.pageY - lastY;
+        var newPrevHeight = prevHeight + diff;
+        var newNextHeight = nextHeight - diff;
+
+        if (newPrevHeight > 10 && newNextHeight > 10) {
+            prevRow.height(newPrevHeight);
+            nextRow.height(newNextHeight);
+        }
+    }
+});
+
+$(document).on("mouseup", function () {
+    isResizingRows = false;
+});
+
+// For resizing the width of the left and right containers
+var isResizingWidth = false;
+var lastX = 0;
+var leftContainerWidth = 0;
+var rightContainerWidth = 0;
+
+$(".resizable-handler").on("mousedown", function (e) {
+    isResizingWidth = true;
+    lastX = e.pageX;
+
+    leftContainerWidth = $("#container-left").width();
+    rightContainerWidth = $("#container-right").width();
+
+    e.preventDefault();
+});
+
+$(document).on("mousemove", function (e) {
+    if (isResizingWidth) {
+        var diff = e.pageX - lastX;
+        var newLeftWidth = leftContainerWidth + diff;
+        var newRightWidth = rightContainerWidth - diff;
+
+        if (newLeftWidth > 100 && newRightWidth > 100) { // Prevent widths too small
+            $("#container-left").width(newLeftWidth);
+            $("#container-right").width(newRightWidth);
+
+            // Update the position of the resizable handler after moving
+            $(".resizable-handler").css("left", newLeftWidth + "px");
+        }
+    }
+});
+
+$(document).on("mouseup", function () {
+    isResizingWidth = false;
+});
--- resources/views/codepen/create.blade.php
+++ resources/views/codepen/create.blade.php
@@ -1,32 +1,30 @@
 <x-app-layout>
-    <div class="flex">
+    <div id="main-container" class="flex w-full m-0 relative" style="height: calc(100vh - 98px)">
         <!-- Editor Containers -->
-        <div class="flex code-panel" style="height: calc(100vh - 80px)">
-            <div class="h-full flex flex-col min-w-96">
-                <div>
-                    <h3 class="text-xl font-semibold">HTML</h3>
-                    <div id="html-editor" class="h-64 border rounded"></div>
-                </div>
-                <div>
-                    <h3 class="text-xl font-semibold">CSS</h3>
-                    <div id="css-editor" class="h-64 border rounded"></div>
-                </div>
-                <div>
-                    <h3 class="text-xl font-semibold">JavaScript</h3>
-                    <div id="js-editor" class="h-64 border rounded"></div>
-                </div>
+        <div id="container-left" class="w-[450px] h-full flex flex-col">
+            <div class="px-2 h-8 flex items-center justify-between">
+                <span class="font-semibold text-md text-black">HTML</span>
             </div>
-            <div class="splitter w-2 h-full bg-slate-300"></div> 
+            <div id="html-editor" class="editor border rounded"></div>
+            <div class="px-2 h-8 flex items-center justify-between cursor-row-resize handler">
+                <span class="font-semibold text-md text-black">CSS</span>
+            </div>
+            <div id="css-editor" class="editor border rounded"></div>
+            <div class="px-2 h-8 flex items-center justify-between cursor-row-resize handler">
+                <span class="font-semibold text-md text-black">JS</span>
+            </div>
+            <div id="js-editor" class="editor border rounded"></div>
         </div>
-
-        <!-- Output Container -->
-        <iframe id="result" class="w-full border rounded bg-white h-[500px]"></iframe>
+        <div id="container-right" class="h-full flex-1">
+            <iframe id="result" class="w-full h-full border rounded bg-white"></iframe>
+        </div>
     </div>
     <div class="bg-white w-full h-8 flex justify-end py-1 px-4">
         <button class="rounded px-3 border border-purple-500 bg-purple-500 text-white text-sm" id="save-btn">Save</button>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-html.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-css.js"></script>
