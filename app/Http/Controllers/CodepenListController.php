47ddef4806acc4ad73c47f2cf0227ace09aaf8e7
danielmong <danmon711@gmail.com>
2024-11-28 23:39:32 -0800

    add codepen route

---
+++ app/Http/Controllers/CodepenListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class CodepenListController extends Controller
+{
+    public function index()
+    {
+        return view('/codepen/list');
+    }
+}
